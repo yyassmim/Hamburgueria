bae7914dfdecea8b21cc1b526c79e6067c7ab345
yyassmim <contatoyasmimap@gmail.com>
2024-07-15 21:11:24 -0300

    uptade

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 
+import { v4 } from "uuid";
+
+import User from "./app/models/user";
+
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.status(200).json({ message: 'Hello World' });
+routes.get('/', async (request, response) => {
+ const user = await User.create({
+  id: v4(),
+  name : "Yasmim",
+  email: "yaasmimaparecida@gmail.com",
+  password: "abcde"
+ })
+
+ return response.status(201).json(user);
+
 });
 
  export default routes;
